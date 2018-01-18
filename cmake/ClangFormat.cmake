fcaa2f31666ef905c085936959876e70c21f4d9a
Armin Novak <armin.novak@thincast.com>
2018-01-18 10:18:45 +0100

    Restricted clang-format versions to 6.0 and up

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -1,25 +1,22 @@
 # get all project files
 file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.c *.h *.m *.java)
 
-find_program(GIT
-	NAMES
-	git)
-
 find_program(CLANG_FORMAT
 	NAMES
+	clang-format-8.0
+	clang-format-7.0
+	clang-format-6.0
 	clang-format
-	clang-format-3.7
-	clang-format-3.8
-	clang-format-3.9
-	clang-format-4.0
-	clang-format-5.0
-	clang-format-6.0)
+	)
 
-add_custom_target(
-        clangformat
-        COMMAND ${CLANG_FORMAT}
-        -style=file
-        -i
-        ${ALL_SOURCE_FILES}
-)
-
+if (NOT CLANG_FORMAT)
+	message(WARNING "clang-format not found in path! code format target not available.")
+else()
+	add_custom_target(
+		clangformat
+		COMMAND ${CLANG_FORMAT}
+		-style=file
+		-i
+		${ALL_SOURCE_FILES}
+	)
+endif()
